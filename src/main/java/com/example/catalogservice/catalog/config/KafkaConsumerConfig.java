f7572e06234e90ead82c4c37e5f1e768dbf7cf37
askyclear <askyclear2@gmail.com>
2022-08-25 22:36:05 +0900

    [인프런] SpringCloud로 개발하는 마이크로서비스 애플리케이션(MSA)
    -- spring-config 적용

--- src/main/java/com/example/catalogservice/catalog/config/KafkaConsumerConfig.java
+++ src/main/java/com/example/catalogservice/catalog/config/KafkaConsumerConfig.java
@@ -1,6 +1,5 @@
 package com.example.catalogservice.catalog.config;
 
-import com.fasterxml.jackson.databind.DeserializationConfig;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
